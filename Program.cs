b2f2ff72c9dd1f80dcfca92679983855eed88587
OsamaAyman1 <121193873+OsamaAyman1@users.noreply.github.com>
2022-12-21 21:02:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1, n2, z;
+
+            Console.WriteLine("Enter number1");
+            n1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number2");
+
+            for (n2 = int.Parse(Console.ReadLine()); n2 <= n1; n2++)
+            {
+                for (z = 2; z <= n2; z++)
+                    if (n2 % z == 0)
+                    {
+                        break;
+                    }
+                if (n2 == z)
+                {
+                    Console.WriteLine(z);
+                }
+                
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
